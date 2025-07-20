875dde0533f7d6200adedf4ba2a4ccf5bd63079c
Nithin <70070522+Nithin-532@users.noreply.github.com>
2025-07-20 15:50:40 +0530

    Update fib.rs

--- fib.rs
+++ fib.rs
@@ -1,18 +1,17 @@
 fn main() {
-    let x: i32 = 46;
-    println!("{}", fib(x));
+    println!("{}", fib(3));
 }
 
-fn fib(n: i32) -> i32 {
-    let mut first: i32 = 0;
-    let mut second: i32 = 1;
+fn fib(n: u32) -> u32 {
+    let mut first: u32 = 0;
+    let mut second: u32 = 1;
     if n == 0 {
         return first;
     }
     if n == 1 {
         return second;
     }
-    for i in 1..n - 2 {
+    for _ in 1..n {
         let temp = second;
         second = second + first;
         first = temp;
